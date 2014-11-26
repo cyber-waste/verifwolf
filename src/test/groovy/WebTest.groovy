79c8c1f1ec0f27d30bfc419c719449051fc5b2e5
yaroslav.yermilov <yermilov17@gmail.com>
2014-11-26 02:16:21 +0200

    task #4

--- src/test/groovy/WebTest.groovy
+++ src/test/groovy/WebTest.groovy
@@ -1,9 +1,25 @@
 import geb.Page
 import geb.spock.GebReportingSpec
+import spock.lang.Stepwise
 
+@Stepwise
 class WebTest extends GebReportingSpec {
 
-    def 'web test'() {
+    def 'web test with non-existing data'() {
+        when:
+        to MainPage
+
+        and:
+        $('form').with {
+            searchRequest = 'non-existing data'
+            $('#submit').click()
+        }
+
+        then:
+        $('#result').text() == 'Result: -1'
+    }
+
+    def 'web test with existing data'() {
         when:
             to MainPage
 
